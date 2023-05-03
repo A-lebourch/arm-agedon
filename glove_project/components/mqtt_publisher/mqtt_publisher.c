40abdce3b49739176ab7ef6b99bd2687400a8f35
TheoDacosta <theo.dacosta@ynov.com>
2023-05-03 11:09:12 +0200

    ready for test with map

--- glove_project/components/mqtt_publisher/mqtt_publisher.c
+++ glove_project/components/mqtt_publisher/mqtt_publisher.c
@@ -1,4 +1,4 @@
-#include"mqtt_publisher.h"
+#include "mqtt_publisher.h"
 #include "driver/adc.h"
 // #include "accel.h"
 // #include "potentiometre.h"
@@ -8,7 +8,8 @@ const char *TAG = "MQTT_EXAMPLE";
 esp_mqtt_client_handle_t client;
 void log_error_if_nonzero(const char *message, int error_code)
 {
-    if (error_code != 0) {
+    if (error_code != 0)
+    {
         ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
     }
 }
@@ -20,7 +21,8 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
     esp_mqtt_client_handle_t client = event->client;
 
     int msg_id;
-    switch ((esp_mqtt_event_id_t)event_id) {
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
         msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "wala", 0, 1, 0);
@@ -54,14 +56,15 @@ void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event
         ESP_LOGI(TAG, "MQTT_EVENT_DATA");
         printf("TOPIC=%ld\r\n", event->topic_len, event->topic);
         printf("DATA=%.*s\r\n", event->data_len, event->data);
-        
+
         break;
     case MQTT_EVENT_ERROR:
         ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT) {
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+        {
             log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
             log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
-            log_error_if_nonzero("captured as transport's socket errno",  event->error_handle->esp_transport_sock_errno);
+            log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
             ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
         }
         break;
@@ -79,15 +82,20 @@ void mqtt_app_start(void)
 #if CONFIG_BROKER_URL_FROM_STDIN
     char line[128];
 
-    if (strcmp(mqtt_cfg.broker.address.uri, "FROM_STDIN") == 0) {
+    if (strcmp(mqtt_cfg.broker.address.uri, "FROM_STDIN") == 0)
+    {
         int count = 0;
         printf("Please enter url of mqtt broker\n");
-        while (count < 128) {
+        while (count < 128)
+        {
             int c = fgetc(stdin);
-            if (c == '\n') {
+            if (c == '\n')
+            {
                 line[count] = '\0';
                 break;
-            } else if (c > 0 && c < 127) {
+            }
+            else if (c > 0 && c < 127)
+            {
                 line[count] = c;
                 ++count;
             }
@@ -95,7 +103,9 @@ void mqtt_app_start(void)
         }
         mqtt_cfg.broker.address.uri = line;
         printf("Broker url: %s\n", line);
-    } else {
+    }
+    else
+    {
         ESP_LOGE(TAG, "Configuration mismatch: wrong broker url");
         abort();
     }
@@ -109,14 +119,15 @@ void mqtt_app_start(void)
 
 void send_value(int motor, int angle)
 {
+
     char motor_id[10];
     sprintf(motor_id, "%u", motor);
     char topic[15] = "/moteur/";
     strcat(topic, motor_id);
     char angle_str[10];
     sprintf(angle_str, "%u", angle);
-    printf("topic : %s\nvalue : %s\n",topic,angle_str);
-    
+    printf("topic : %s\nvalue : %s\n", topic, angle_str);
+
     esp_mqtt_client_publish(client, topic, angle_str, 0, 0, 0);
 }
 
@@ -144,8 +155,7 @@ void mqtt_init(void)
     // xTaskCreate(&my_task, "my_task", 4096, NULL, 1, NULL);
 }
 
-
-   void my_task()
+void my_task()
 {
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC1_CHANNEL_3, ADC_ATTEN_DB_11);
@@ -154,7 +164,6 @@ void mqtt_init(void)
     adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_11);
     adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
 
-
     while (1)
     {
         int potentiometer_value3 = adc1_get_raw(ADC1_CHANNEL_3);
@@ -162,11 +171,11 @@ void mqtt_init(void)
         int potentiometer_value5 = adc1_get_raw(ADC1_CHANNEL_5);
         int potentiometer_value6 = adc1_get_raw(ADC1_CHANNEL_6);
         int potentiometer_value7 = adc1_get_raw(ADC1_CHANNEL_7);
-        send_value(3,potentiometer_value3);
-        send_value(4,potentiometer_value4);
-        send_value(5,potentiometer_value5);
-        send_value(6,potentiometer_value6);
-        send_value(7,potentiometer_value7);
+        send_value(3, potentiometer_value3);
+        send_value(4, potentiometer_value4);
+        send_value(5, potentiometer_value5);
+        send_value(6, potentiometer_value6);
+        send_value(7, potentiometer_value7);
         vTaskDelay(500 / portTICK_PERIOD_MS);
     }
 }
@@ -201,6 +210,5 @@ void mqtt_init(void)
 //         // Attendre un certain temps avant de lire à nouveau la valeur du potentiomètre
 //         vTaskDelay(pdMS_TO_TICKS(500));
 //     }
-
 
 // }
